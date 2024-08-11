e1b9d0b46f0d0e9ae01d3283bf661279a029d1f4
Michael Tichý <michael.tichy@sanasport.cz>
2024-08-11 12:30:05 +0200

    Homepage: added product test data for preview

--- src/Controller/Homepage/HomepageController.php
+++ src/Controller/Homepage/HomepageController.php
@@ -3,14 +3,46 @@
 namespace App\Controller\Homepage;
 
 use App\Controller\BaseController;
+use App\Service\Product\ProductViewsCounter;
 use Symfony\Component\HttpFoundation\Response;
 
 final class HomepageController extends BaseController
 {
 
+	private ProductViewsCounter $productViewsCounter;
+
+	public function __construct(ProductViewsCounter $productViewsCounter)
+	{
+		$this->productViewsCounter = $productViewsCounter;
+	}
+
 	public function default(): Response
 	{
-		return $this->render('Homepage/templates/homepageDefault.html.twig');
+		return $this->render(
+			'Homepage/templates/homepageDefault.html.twig',
+			[
+				'products' => $this->getTestProductData(),
+				'productViewCounts' => $this->productViewsCounter->getAllCounts(),
+			],
+		);
+	}
+
+	/**
+	 * Test data
+	 *
+	 * @return array<int, array<string, mixed>>
+	 */
+	private function getTestProductData(): array
+	{
+		$products = [];
+		for ($i = 1; $i <= 10; $i++) {
+			$products[] = [
+				'id' => $i,
+				'name' => sprintf('Product %d', $i),
+			];
+		}
+
+		return $products;
 	}
 
 }
