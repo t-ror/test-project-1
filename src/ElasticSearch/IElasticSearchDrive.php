107da8161f07196010885f5247859bb1820357ef
Michael Tichý <michael.tichy@sanasport.cz>
2024-08-11 13:31:21 +0200

    ElasticSearchDrive: fixed comment

--- src/ElasticSearch/IElasticSearchDrive.php
+++ src/ElasticSearch/IElasticSearchDrive.php
@@ -3,9 +3,8 @@
 namespace App\ElasticSearch;
 
 /**
- * Metodu findProduct() jsem dal pod IRepository jako findById()
- *
- * Kvůli jedonduchosti ponechávám custom connection (drive) do databáze jinak bych využil Doctrine.
+ * Metodu findById() jsem dal pod IRepository
+
  * Metoda jen pro ukázku aby bylo jasné k čemu interface slouží.
  */
 interface IElasticSearchDrive
